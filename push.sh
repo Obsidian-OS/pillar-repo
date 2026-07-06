baa2c85908f703be68c605bf939c338e546b9560
neoapps-dev <asd22.info@gmail.com>
2026-07-06 18:24:44 +0300

    plugind

--- push.sh
+++ push.sh
@@ -2,5 +2,5 @@
 cloudflared access tcp --hostname ssh.obsidianos.xyz --listener 127.0.0.1:2222 &
 PID=$!
 sleep 1
-rsync -av --include-from=.sftpinclude --exclude="*" -e 'ssh -p 2222' ./ localhost:/var/www/html/pillar/
+rsync -av --exclude="src/*/src" --include-from=.sftpinclude --exclude="*" -e 'ssh -p 2222' ./ localhost:/var/www/html/pillar/
 kill $PID
